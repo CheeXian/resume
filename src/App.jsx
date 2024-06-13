9073d54d4e6a000a89f2ec5f2df7fb3cb5b08bfc
cheexian <cheexianyau@gmail.com>
2024-06-13 19:26:01 +0800

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@
 function App() {
   return (
     <>
-      <h1>Haris Samingan</h1>
-      <h2>Software developer</h2>
+      <h1>Yau Chee Xian</h1>
+      <h2>Software developers</h2>
     </>
   )
 }
